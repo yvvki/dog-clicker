62f4da3118e5d6967c10df17cfdb3ffbec12bcc8
Yuuki Rika <yvvki@kekkon.nexus>
2025-12-13 23:27:03 +0700

    feat: audio

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_soloud/flutter_soloud.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(const ClickerApp());
+  runApp(ClickerApp());
 }
 
 class ClickerApp extends StatelessWidget {
@@ -36,7 +36,10 @@ class ClickerApp extends StatelessWidget {
                   ),
             useMaterial3: true,
           ),
-          home: const ClickerHomePage(),
+          home: ClickerHomePage(
+            assetDown: 'assets/clicker-down.wav',
+            assetUp: 'assets/clicker-up.wav',
+          ),
         );
       },
     );
@@ -44,15 +47,82 @@ class ClickerApp extends StatelessWidget {
 }
 
 class ClickerHomePage extends StatefulWidget {
-  const ClickerHomePage({super.key});
+  const ClickerHomePage({
+    super.key,
+    required this.assetDown,
+    required this.assetUp,
+  });
+
+  final String assetDown;
+  final String assetUp;
 
   @override
   State<ClickerHomePage> createState() => _ClickerHomePageState();
 }
 
 class _ClickerHomePageState extends State<ClickerHomePage> {
+  late final SoLoud _soloud;
+
+  late final AudioSource _audioDown;
+  late final AudioSource _audioUp;
+
+  bool _isTapped = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      _soloud = SoLoud.instance;
+      await _soloud.init(bufferSize: 512);
+      _soloud.setGlobalVolume(1.5); // No way to disable CLIP_ROUNDOFF
+
+      _audioDown = await _soloud.loadAsset(widget.assetDown);
+      _audioUp = await _soloud.loadAsset(widget.assetUp);
+    });
+  }
+
+  @override
+  void dispose() {
+    _soloud.deinit();
+    super.dispose();
+  }
+
+  void _down() {
+    if (_isTapped) return;
+    setState(() => _isTapped = true);
+    _soloud.play(_audioDown);
+  }
+
+  void _up() {
+    if (!_isTapped) return;
+    setState(() => _isTapped = false);
+    _soloud.play(_audioUp);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center());
+    final diameter = MediaQuery.of(context).size.shortestSide * 0.5;
+
+    return Scaffold(
+      body: Center(
+        child: ClipOval(
+          child: Listener(
+            onPointerDown: (_) => _down(),
+            onPointerUp: (_) => _up(),
+            onPointerCancel: (_) => _up(),
+            child: AnimatedContainer(
+              duration: Durations.short1,
+              width: diameter,
+              height: diameter,
+              decoration: BoxDecoration(
+                color: !_isTapped
+                    ? Theme.of(context).colorScheme.primary
+                    : Theme.of(context).colorScheme.primaryContainer,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
 import dynamic_color
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
